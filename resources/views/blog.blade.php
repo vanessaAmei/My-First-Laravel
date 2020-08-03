04f9f2e6491c71dc6af59cec39ab81fc035ff8f2
Vanessa <vanessangela11@gmail.com>
2020-08-03 13:08:53 +0700

    Laravel First

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog 1 </title>
+</head>
+<body>
+ 
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+ 
+</body>
+</html>
